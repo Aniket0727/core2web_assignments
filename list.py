8c3efd415e45fbb4c246b79809d1ae74166b45db
Aniket Kangude <142888993+Aniket0727@users.noreply.github.com>
2024-02-28 18:40:27 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,17 @@
+l1=[num for num in range(1,10)]
+# print(l1)
+
+l2=["Annu","Aniket",["Omkar","Yash"]]
+print(l2)
+
+
+l2[1]="Rushi"
+
+print(l2)
+
+
+print("aaa")
+print("aaa")
+
+if(5<9):
+    pass
