f57757164cf03ef7c68ec862ed0d87175d170825
Aniket27 <aniketkangude9060@gmail.com>
2024-02-29 14:23:41 +0530

    new update

--- list.py
+++ list.py
@@ -9,18 +9,28 @@
 
 # print(l2)
 
-l3=["Java","Python","Programing in c",["HTML","PHP","CSS"]]
+# l3=["Java","Python","Programing in c",["HTML","PHP","CSS"]]
 
-l4=l3.copy()
+# l4=l3.copy()
 
-print("l3: ",l3)
-print("l4: ",l4)
+# print("l3: ",l3)
+# print("l4: ",l4)
 
-l3[3][1]="BOOTSTRAP"
+# l3[3][1]="BOOTSTRAP"
+# print("After Change ")
+# print("l3: ",l3)
+# print("l4: ",l4)
+
+
+l5=["Java","Python","Programing in c","HTML","PHP","CSS"]
+
+l6=l5.copy()
+
+print("l3: ",l5)
+print("l4: ",l6)
+
+l5[3]="BOOTSTRAP"
 print("After Change ")
-print("l3: ",l3)
-print("l4: ",l4)
-
-print(id(l3))
-print(id(l4))
+print("l3: ",l5)
+print("l4: ",l6)
 
