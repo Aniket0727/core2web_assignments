5274c78ed21c77edeef32b437a413659408b58ee
aniket0727 <aniketkangude0727@gmail.com>
2024-03-01 12:34:20 +0530

    dictionary

--- tuple.py
+++ tuple.py
@@ -2,6 +2,7 @@ tp=("Annu",27,0.5,("Java","Python"),27)
 # print(tp[3][1])
 print(tp)
 
-tp[3][1]="HTML" # tuple can not be change
-print(tp)
+# tp[3][1]="HTML" # tuple can not be change
+# print(tp)
+
 
